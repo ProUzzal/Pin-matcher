910a5f1a3391022f2e3271385b0956afaf70d9a1
Uzzal Ahmed <61231062+UzzalMia@users.noreply.github.com>
2020-08-23 17:27:51 +0600

    added html css and js file

---
+++ index.js
@@ -0,0 +1,47 @@
+//Pin generate Part
+document.querySelector("#generate-pin").addEventListener("click", function () {
+  const generatedPin = Math.floor(Math.random() * 9000) + 1000;
+  document.querySelector("#generate-screen").value = generatedPin;
+});
+
+// Button Capturing Part
+document.getElementById("cal-body").addEventListener("click", function (e) {
+  const inputButton = e.target.innerText;
+  const calScreen = parseInt(document.getElementById("cal-screen").value);
+  if (inputButton < 10) {
+    document.getElementById("cal-screen").value += inputButton;
+  } else if (inputButton == "C") {
+    document.getElementById("cal-screen").value = "";
+  } else if (inputButton == "<") {
+    document.getElementById("cal-screen").value = document
+      .getElementById("cal-screen")
+      .value.slice(0, -1);
+  }
+});
+
+//Submit
+
+document.getElementById("submitId").addEventListener("click", function () {
+  const generateScreen = document.querySelector("#generate-screen").value;
+  const calScreen = document.getElementById("cal-screen").value;
+
+  if (calScreen == generateScreen) {
+    document.getElementById("success-message").style.display = "block";
+    document.getElementById("error-message").style.display = "none";
+  } else {
+    // for (let i = 1; i < 3; i++) {
+
+    document.getElementById("error-message").style.display = "block";
+    document.getElementById("success-message").style.display = "none";
+
+    //
+    let message = parseInt(document.getElementById("action-number").innerText);
+    let minusValue = message--;
+    console.log(minusValue);
+    if (minusValue == 0) {
+      document.getElementById("submitId").disabled = true;
+      document.getElementById("action-number").innerText = minusValue;
+    }
+    // }
+  }
+});
